d8881ac3a1ea22b8e6a288ab8890ee87feb253d3
jschaul <jschaul@users.noreply.github.com>
2020-04-28 17:22:08 +0200

    upgrade 0.12

--- outputs.tf
+++ outputs.tf
@@ -4,32 +4,44 @@
 
 output "created_delegation_set_id" {
   description = "The ID of the shared delegation set applied to all zones that has been created by this module if it was specified by name."
-  value       = "${element(concat(aws_route53_delegation_set.new.*.id, list("")), 0)}"
+  value       = element(concat(aws_route53_delegation_set.new.*.id, [""]), 0)
 }
 
 output "created_delegation_set_name" {
   description = "The name of the shared delegation set applied to all zones that has been created by this module if it was specified by name."
-  value       = "${element(concat(aws_route53_delegation_set.new.*.reference_name, list("")), 0)}"
+  value = element(
+    concat(aws_route53_delegation_set.new.*.reference_name, [""]),
+    0,
+  )
 }
 
 output "created_delegation_set_name_servers" {
   description = "A list of name servers of the shared delegation set applied to all zones that has been created by this module if it was specified by name."
-  value       = ["${flatten(aws_route53_delegation_set.new.*.name_servers)}"]
+  value       = [flatten(aws_route53_delegation_set.new.*.name_servers)]
 }
 
 output "existing_delegation_set_id" {
   description = "The ID of the shared delegation set applied to all zones that alreday existed and was specified by its ID."
-  value       = "${element(concat(data.aws_route53_delegation_set.existing.*.id, list("")), 0)}"
+  value = element(
+    concat(data.aws_route53_delegation_set.existing.*.id, [""]),
+    0,
+  )
 }
 
 output "existing_delegation_set_name" {
   description = "The name of the shared delegation set applied to all zones that alreday existed and was specified by its ID."
-  value       = "${element(concat(data.aws_route53_delegation_set.existing.*.reference_name, list("")), 0)}"
+  value = element(
+    concat(
+      data.aws_route53_delegation_set.existing.*.reference_name,
+      [""],
+    ),
+    0,
+  )
 }
 
 output "existing_delegation_set_name_servers" {
   description = "A list of name servers of the shared delegation set applied to all zones that alreday existed and was specified by its ID."
-  value       = ["${flatten(data.aws_route53_delegation_set.existing.*.name_servers)}"]
+  value       = [flatten(data.aws_route53_delegation_set.existing.*.name_servers)]
 }
 
 # -------------------------------------------------------------------------------------------------
@@ -38,28 +50,35 @@ output "existing_delegation_set_name_servers" {
 
 output "public_zones_delegation_set_id" {
   description = "The ID of the shared delegation set applied to all zones that is actually used by the zones."
-  value       = "${element(concat(aws_route53_delegation_set.new.*.id, data.aws_route53_delegation_set.existing.*.id, list("")), 0)}"
+  value = element(
+    concat(
+      aws_route53_delegation_set.new.*.id,
+      data.aws_route53_delegation_set.existing.*.id,
+      [""],
+    ),
+    0,
+  )
 }
 
 output "public_zones" {
   description = "List of created public zones."
 
   value = [
-    "${concat(
+    concat(
       aws_route53_zone.name_delegated_public_domains.*.name,
       aws_route53_zone.id_delegated_public_domains.*.name,
       aws_route53_zone.undelegated_public_domains.*.name,
       aws_route53_zone.name_delegated_public_subdomains.*.name,
       aws_route53_zone.id_delegated_public_subdomains.*.name,
       aws_route53_zone.undelegated_public_subdomains.*.name,
-    )}",
+    ),
   ]
 }
 
 output "public_zones_ids" {
   description = "List of zone-id mappings for created public zones."
 
-  value = "${concat(
+  value = concat(
     formatlist(
       "{\"%v\": \"%v\"}",
       aws_route53_zone.name_delegated_public_domains.*.name,
@@ -90,7 +109,7 @@ output "public_zones_ids" {
       aws_route53_zone.undelegated_public_subdomains.*.name,
       aws_route53_zone.undelegated_public_subdomains.*.id,
     ),
-  )}"
+  )
 }
 
 #output "public_zones_name_servers" {
@@ -128,4 +147,3 @@ output "public_zones_ids" {
 #    ),
 #  )}"
 #}
-
